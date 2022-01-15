e446b89f7c4d151ee2993ed37519b8a079b49ca2
Silgfrid <cristian1984mendez@gmail.com>
2022-01-15 15:48:11 -0300

    primero commit

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,31 @@
+import { StringMap } from "@angular/compiler/src/compiler_facade_interface";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+
+})
+
+
+export class HeroeComponent{
+
+    nombre: string = 'ironman';
+    edad: number = 45;
+
+    get nombreCapitalizado(): string { 
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string{
+        return ` ${this.nombre} - ${this.edad} `;
+    }
+
+    cambiarNombre(): void {
+        this.nombre = 'Spiderman';
+    }
+
+    cambiarEdad(): void {
+        this.edad = 10;
+    }
+}
